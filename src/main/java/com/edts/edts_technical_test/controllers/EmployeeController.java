5ac28f83df1f033a22a1b14d53e6363566e3275a
Edwin Hadisumitro <edwinhadisumitro.tech@gmail.com>
2021-10-11 11:56:51 +0700

    Fix error response

--- src/main/java/com/edts/edts_technical_test/controllers/EmployeeController.java
+++ src/main/java/com/edts/edts_technical_test/controllers/EmployeeController.java
@@ -2,16 +2,18 @@ package com.edts.edts_technical_test.controllers;
 
 import java.math.BigInteger;
 
-import com.edts.edts_technical_test.constants.exception.EmployeeException;
+import com.edts.edts_technical_test.constants.exception.EmployeeNotFoundException;
 import com.edts.edts_technical_test.interfaces.employee.EmployeeRepository;
 import com.edts.edts_technical_test.models.Employee;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class EmployeeController {
@@ -24,7 +26,7 @@ public class EmployeeController {
 
     @GetMapping("/employee/{id}")
     public Employee get(@PathVariable int id) {
-        return this.employeeRepository.findById(BigInteger.valueOf(id)).orElseThrow(() -> EmployeeException.EmployeeNotFoundException());
+        return this.employeeRepository.findById(BigInteger.valueOf(id)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found"));
     }
 
     @PostMapping("/employee")
@@ -39,6 +41,6 @@ public class EmployeeController {
             existingEmployee.setGrade(employee.getGrade());
             existingEmployee.setSalary(employee.getSalary());
             return employeeRepository.save(existingEmployee);
-        }).orElseThrow(() -> EmployeeException.EmployeeNotFoundException());
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found"));
     }
 }
